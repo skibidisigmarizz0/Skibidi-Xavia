26359cf1d72dae62660d49818993acdd627d627c
skibidi <jimmuelrivera144@gmail.com>
2024-12-06 03:55:33 +0800

    Update showPrefix.js

--- plugins/onMessage/showPrefix.js
+++ plugins/onMessage/showPrefix.js
@@ -1,17 +1,23 @@
+import axios from 'axios';
+
 const langData = {
     "en_US": {
-        "prefix.get": "Prefix is: {prefix}"
-    },
-    "vi_VN": {
-        "prefix.get": "Prefix hiện tại là: {prefix}"
+        "prefix.get": "𝐃𝐚́𝐱𝐨̈𝐧 𝚙𝚛𝚎𝚏𝚒𝚡 𝚒𝚜: {prefix}"
     }
 }
 
-function onCall({ message, getLang, data }) {
-    if (message.body == "prefix" && message.senderID != global.botID) {
-        message.reply(getLang("prefix.get", {
-            prefix: data?.thread?.data?.prefix || global.config.PREFIX
-        }));
+async function onCall({ message, getLang, data }) {
+
+    if (message.body == "prefix" && message.senderID !== global.botID) {
+        const attachment = await axios.get("https://imgur.com/4RigCGl.jpeg", { responseType: "stream" })
+            .then(response => response.data)
+            .catch(err => console.error("Error fetching image: ", err));
+        message.reply({
+            body: getLang("prefix.get", {
+                prefix: data?.thread?.data?.prefix || global.config.PREFIX
+            }),
+            attachment: attachment 
+        });
     }
 
     return;
